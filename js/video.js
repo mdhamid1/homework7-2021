7cafb4bf37fd2dcbf6343736b73471bb9e9f86d5
mdhamid1 <76439219+mdhamid1@users.noreply.github.com>
2021-04-01 18:10:59 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,73 @@
+var video = document.querySelector("#player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	// volume.innterHTML= 100;
+	video.play();
+	// document.querySelector("#volume").innerHTML=video.volume * 100 + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow down video");
+	video.playbackRate *= 0.95;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speeding up video");
+	video.playbackRate /= 0.95;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	console.log("Changing volume");
+	console.log(this.value);
+	video.volume = this.value/100;
+	document.querySelector("#volume").innerHTML=video.volume*100 + "%";
+});
+
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Changing CSS");
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("Changing CSS");
+	video.classList.remove("oldSchool");
+});
+
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime += video.currentTime + 15;
+	video.addEventListener("ended", myFunc, false);
+	function myFunc(e) {
+		video.load()
+	}
+	console.log("skipping video: " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted === false) {    
+           video.muted = true;
+           document.querySelector("#mute").innerHTML="unmute";
+           document.querySelector("#volume").innerHTML="0%";
+    }
+
+    else {
+        video.muted = false;
+        document.querySelector("#mute").innerHTML="mute";
+        document.querySelector("#volume").innerHTML=video.volume*100 + "%";
+    }
+});
